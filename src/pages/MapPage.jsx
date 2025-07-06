bdfc2367f8ec5e8ebab69b90b0e93c4de25869b6
SardorQobilov <sardorqobilov555@gmail.com>
2025-07-06 19:15:13 +0500

    map page added

---
+++ src/pages/MapPage.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+export const MapPage = () => {
+  return (
+    <div className='space-y-5 text-3xl'>
+      <Link className='text-primary underline' to='/home'>Home</Link>
+    </div>
+  );
+};
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
 // routes.jsx
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
+import { MapPage } from './pages/MapPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path='/' element={<MapPage />} />
       <Route path='/home' element={<HomePage />} />
     </Routes>
   );
