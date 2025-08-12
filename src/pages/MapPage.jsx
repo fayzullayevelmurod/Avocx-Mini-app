d3f0c5f1c42acb95856f245b81b195f351a8a447
QobilovSardor <note47297@gmail.com>
2025-08-12 11:35:27 +0500

    map page sort

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,37 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const MapPage = () => {
   const routes = [
-    { path: '/home', label: 'Home' },
-    { path: '/my-shops', label: 'My Shops' },
-    { path: '/my-shops-two', label: 'My Shops 2' },
-    { path: '/create-shop', label: 'Create Shop' },
-    { path: '/store-management', label: 'Store Management' },
-    { path: '/store-management-2', label: 'Store Management 2' },
-    { path: '/product-card', label: 'Product Card' },
-    { path: '/product-card-two', label: 'Product Card 2' },
-    { path: '/shops', label: 'Shops' },
-    { path: '/basket', label: 'Basket' },
-    { path: '/my-orders', label: 'My Orders' },
-    { path: '/announcement', label: 'Announcement' },
-    { path: '/announcement-two', label: 'Announcement 2' },
-    { path: '/create-ad', label: 'Create Ad' },
-    { path: '/create-ad-two', label: 'Create Ad 2' },
-    { path: '/create-ad-success', label: 'Create Ad Success' },
-    { path: '/loading', label: 'Loading' },
-    { path: '/not-found', label: 'NotFound' },
-    { path: '/welcome', label: 'Welcome' },
+    { path: "/loading", label: "Loading" },
+    { path: "/home", label: "Home" },
+    { path: "/create-shop", label: "Create Shop" },
+    { path: "/product-card", label: "Product Card" },
+    { path: "/announcement", label: "Announcement" },
+    { path: "/products-my-stores", label: "Products My Stores" },
+    { path: "/create-ad-success", label: "Create Ad Success" },
+    { path: "/my-shop-not-found", label: "My Shop Not Found" },
   ];
 
   return (
-    <div className='space-y-5 text-3xl'>
+    <div className="space-y-5 text-3xl">
       {routes.map((route, index) => (
         <Link
           key={index}
-          className='text-primary block underline'
+          className="text-primary block underline"
           to={route.path}
         >
-          {route.label}
+          {`${index + 1}: ${route.label}`}
         </Link>
       ))}
     </div>
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,53 +1,56 @@
 // routes.jsx
-import { Routes, Route } from 'react-router-dom';
-import { MapPage } from './pages/MapPage';
-import MyShops from './pages/MyShops';
-import MyShopsTwo from './pages/MyShopsTwo';
-import Step from './pages/create-shop/Step';
-import StoreManagement from './pages/StoreManagement';
-import StoreManagementTwo from './pages/StoreManagementTwo';
-import ProductCard from './pages/ProductCard';
-import ProductCardTwo from './pages/ProductCardTwo';
-import Basket from './pages/Basket';
-import MyOrders from './pages/MyOrders';
-import Announcement from './pages/Announcement';
-import AnnouncementTwo from './pages/AnnouncementTwo';
-import CreateAd from './pages/CreateAd';
-import CreateAdTwo from './pages/CreateAdTwo';
-import CreateAdSuccess from './pages/CreateAdSuccess';
-import Shops from './pages/Shops';
-import Home from './pages/Home';
-import Loading from './pages/Loading';
-import NotFound from './pages/NotFound';
-import Welcome from './pages/Welcome';
-import { MyShopNotFound } from './pages/MyShopNotFound';
-import { ProductsMyStores } from './components/ProductsMyStores';
+import { Routes, Route } from "react-router-dom";
+import { MapPage } from "./pages/MapPage";
+import MyShops from "./pages/MyShops";
+import MyShopsTwo from "./pages/MyShopsTwo";
+import Step from "./pages/create-shop/Step";
+import StoreManagement from "./pages/StoreManagement";
+import StoreManagementTwo from "./pages/StoreManagementTwo";
+import ProductCard from "./pages/ProductCard";
+import ProductCardTwo from "./pages/ProductCardTwo";
+import Basket from "./pages/Basket";
+import MyOrders from "./pages/MyOrders";
+import Announcement from "./pages/Announcement";
+import AnnouncementTwo from "./pages/AnnouncementTwo";
+import CreateAd from "./pages/CreateAd";
+import CreateAdTwo from "./pages/CreateAdTwo";
+import CreateAdSuccess from "./pages/CreateAdSuccess";
+import Shops from "./pages/Shops";
+import Home from "./pages/Home";
+import Loading from "./pages/Loading";
+import NotFound from "./pages/NotFound";
+import Welcome from "./pages/Welcome";
+import { MyShopNotFound } from "./pages/MyShopNotFound";
+import { ProductsMyStores } from "./components/ProductsMyStores";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<MapPage />} />
-      <Route path='/shops' element={<Shops />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/my-shops' element={<MyShops />} />
-      <Route path='/my-shops-two' element={<MyShopsTwo />} />
-      <Route path='/create-shop' element={<Step />} />
-      <Route path='/store-management' element={<StoreManagement />} />
-      <Route path='/store-management-2' element={<StoreManagementTwo />} />
-      <Route path='/product-card' element={<ProductCard />} />
-      <Route path='/product-card-two' element={<ProductCardTwo />} />
-      <Route path='/basket' element={<Basket />} />
-      <Route path='/my-orders' element={<MyOrders />} />
-      <Route path='/announcement' element={<Announcement />} />
-      <Route path='/announcement-two' element={<AnnouncementTwo />} />
-      <Route path='/create-ad' element={<CreateAd />} />
-      <Route path='/create-ad-two' element={<CreateAdTwo />} />
-      <Route path='/create-ad-success' element={<CreateAdSuccess />} />
-      <Route path='/loading' element={<Loading />} />
-      <Route path='/not-found' element={<NotFound />} />
-      <Route path='/welcome' element={<Welcome />} />
-      <Route path='/my-shop-not-found' element={<MyShopNotFound />} />
-      <Route path='/products-my-stores' element={<ProductsMyStores />} />
+      {/* finished pages */}
+      <Route path="/home" element={<Home />} />
+      <Route path="/create-shop" element={<Step />} />
+      <Route path="/product-card" element={<ProductCard />} />
+      <Route path="/announcement" element={<Announcement />} />
+      <Route path="/products-my-stores" element={<ProductsMyStores />} />
+      <Route path="/loading" element={<Loading />} />
+      <Route path="/create-ad-success" element={<CreateAdSuccess />} />
+      <Route path="/my-shop-not-found" element={<MyShopNotFound />} />
+      {/* finished pages */}
+
+      <Route path="/" element={<MapPage />} />
+      <Route path="/shops" element={<Shops />} />
+      <Route path="/my-shops" element={<MyShops />} />
+      <Route path="/my-shops-two" element={<MyShopsTwo />} />
+      <Route path="/store-management" element={<StoreManagement />} />
+      <Route path="/store-management-2" element={<StoreManagementTwo />} />
+      <Route path="/product-card-two" element={<ProductCardTwo />} />
+      <Route path="/basket" element={<Basket />} />
+      <Route path="/my-orders" element={<MyOrders />} />
+      <Route path="/announcement-two" element={<AnnouncementTwo />} />
+      <Route path="/create-ad" element={<CreateAd />} />
+      <Route path="/create-ad-two" element={<CreateAdTwo />} />
+      <Route path="/not-found" element={<NotFound />} />
+      <Route path="/welcome" element={<Welcome />} />
     </Routes>
   );
 };
